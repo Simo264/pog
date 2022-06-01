5eae35903ae9b06f8cb257039b96a7533b6043af
Simo264 <60814604+Simo264@users.noreply.github.com>
2022-06-01 15:24:09 +0200

    Update ApplicationBuildMode.java

--- src/ApplicationBuildMode.java
+++ src/ApplicationBuildMode.java
@@ -1,5 +1,5 @@
 public class ApplicationBuildMode
 {
     public static enum EBuildModes { WITH_IDE, WITH_MAKEFILE };
-    public final static EBuildModes BUILD_MODE = EBuildModes.WITH_IDE;
+    public final static EBuildModes BUILD_MODE = EBuildModes.WITH_MAKEFILE;
 }

8401520c8c33530b3b174e7e93b56e9037d8a051
simone <simonevaiasinni@gmail.com>
2022-06-01 17:50:37 +0200

    Docs: done!

---
+++ docs/html/search/functions_8.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['update_62',['update',['../classApplicationFileParser.html#afc3b84fc561195f69717d1531c7f4917',1,'ApplicationFileParser.update()'],['../classApplicationFileWrapper.html#a0e65667ac22b784920d2329fe1d5fe31',1,'ApplicationFileWrapper.update()'],['../classApplicationWorkspace.html#a42818522d1769b3ec8abbcd01493f8bd',1,'ApplicationWorkspace.update()']]]
+];
---
+++ docs/html/search/variables_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['workspace_64',['workspace',['../classApplication.html#a7877d1a83d36c2400cb89c84998da912',1,'Application']]]
+];
